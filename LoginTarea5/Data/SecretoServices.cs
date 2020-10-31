3593a3d8adbdf222d87d927ad3a18485e549f207
CarlosSvelti <60992525+CarlosSvelti@users.noreply.github.com>
2020-10-31 00:16:05 -0400

    version 1

---
+++ LoginTarea5/Data/SecretoServices.cs
@@ -0,0 +1,42 @@
+﻿
+using LoginTarea5.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Data
+{
+    public class SecretoServices
+    {
+        private readonly ApplicationDbContext _db;
+        public SecretoServices(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public List<SecretosModel> GetIlegalinfos()
+        {
+            var ilegal = _db.Secretos.ToList();
+            return ilegal;
+        }
+        public string Create(SecretosModel objinfo)
+        {
+            _db.Secretos.Add(objinfo);
+            _db.SaveChanges();
+            return "Guardado";
+        }
+
+        public SecretosModel GetIlegalinfoById(int id)
+        {
+            SecretosModel ilegal = _db.Secretos.FirstOrDefault(s => s.id == id);
+            return ilegal;
+        }
+        public string Deleteinfo(SecretosModel objinfo)
+        {
+            _db.Remove(objinfo);
+            _db.SaveChanges();
+            return "Borrado";
+        }
+    }
+}
---
+++ LoginTarea5/Data/SecretosModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginTarea5.Data
+{
+    public class SecretosModel
+    {
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+        public double valor { get; set; }
+        public DateTime fecha { get; set; }
+        public string lugar { get; set; }
+        public double latitud { get; set; }
+        public double logitud { get; set; }
+    }
+}
